e4b761fa600abab7bb3dcf9618a62c3d69a5a036
artemiev <mixartemev@gmail.com>
2024-03-05 11:14:30 +0300

    user-client merge

--- tortoise_api/api.py
+++ tortoise_api/api.py
@@ -13,7 +13,7 @@ from starlette import status
 from starlette.middleware.cors import CORSMiddleware
 from starlette.requests import Request
 from starlette.types import Lifespan
-from tortoise import Tortoise
+from tortoise import Tortoise, ModelMeta
 from tortoise.contrib.pydantic import PydanticModel
 from tortoise.contrib.starlette import register_tortoise
 from tortoise.exceptions import IntegrityError, DoesNotExist
@@ -21,7 +21,7 @@ from tortoise.exceptions import IntegrityError, DoesNotExist
 from tortoise_api_model.model import Model
 from tortoise_api_model.pydantic import PydList
 
-from tortoise_api.oauth import login_for_access_token, Token, get_current_user, reg_user, read, write, my
+from tortoise_api.oauth import OAuth
 
 
 class ListArgs(BaseModel):
@@ -44,13 +44,16 @@ class Api:
             debug: bool = False,
             title: str = 'FemtoAPI',
             exc_models: set[str] = None,
-            lifespan: Lifespan = None
+            lifespan: Lifespan = None,
+            tg_auth: bool = False
     ):
         """
         Parameters:
             debug: Debug SQL queries, api requests
             # auth_provider: Authentication Provider
         """
+        load_dotenv()
+
         if debug:
             logging.basicConfig(level=logging.DEBUG)
 
@@ -59,20 +62,25 @@ class Api:
         # collect not top (bottom) models for removing
         bottom_models: {Model.__class__} = reduce(lambda x, y: x | set(y[1:]), models_trees.values(), {object}) & set(models_trees)
         # filter only top model names
-        # mm = {m: v for m in dir(module) if isinstance(v:=getattr(module, m), ModelMeta)}
-        # [delattr(module, m.__name__) for m in bottom_models if m in mm.values()]
+        mm = {m: v for m in dir(module) if isinstance(v := getattr(module, m), ModelMeta)}
+        [delattr(module, n) for n, m in mm.items() if m in bottom_models]
         top_models = set(models_trees.keys()) - bottom_models
         # set global models list
         self.models = {m.__name__: m for m in top_models if not exc_models or m.__name__ not in exc_models}
 
         Tortoise.init_models([module], "models")  # for relations
 
-        schemas: {str: (Type[PydanticModel], Type[PydanticModel], Type[PydList])} = {k: (m.pyd(), m.pydIn(), m.pydsList()) for k, m in self.models.items()}
+        schemas: {str: (Type[PydanticModel], Type[PydanticModel], Type[PydList])} = {k: (
+            m.pyd(),
+            m.pydIn(),
+            m.pydsList()
+        ) for k, m in self.models.items()}
 
         # get auth token route
+        self.oauth = OAuth(env('SECRET'), self.models['User'])
         auth_routes = [
-            APIRoute('/register', reg_user, methods=['POST'], tags=['auth'], name='SignUp', response_model=Token),
-            APIRoute('/token', login_for_access_token, methods=['POST'], response_model=Token, tags=['auth'], operation_id='token'),
+            APIRoute('/register', self.oauth.reg_user, methods=['POST'], tags=['auth'], name='SignUp', response_model=self.oauth.Token),
+            APIRoute('/token', self.oauth.login_for_access_token, methods=['POST'], response_model=self.oauth.Token, tags=['auth'], operation_id='token'),
         ]
 
         # main app
@@ -94,12 +102,12 @@ class Api:
                 return self.models[nam]
 
             DynamicListRequestModel = create_model(
-                name+'ListRequest', q=(str, self.models[name]._name), __base__=ListArgs
+                name+'ListRequest', q=(str, ''), __base__=ListArgs
             )
 
             async def index(request: Request, params: DynamicListRequestModel) -> schema[2]:
                 mod: Model.__class__ = _req2mod(request)
-                sorts = ([params.sort] if params.sort else []) + mod._sorts
+                sorts = [params.sort] if params.sort else mod._sorts
                 data = await mod.pagePyd(sorts, params.limit, params.offset, params.q, **params.model_extra)
                 return data
 
@@ -134,13 +142,12 @@ class Api:
 
             ar = APIRouter(routes=[
                 APIRoute('/'+name, index, methods=['POST'], name=name+' objects list', response_model=schema[2]),
-                APIRoute('/'+name, upsert, methods=['PUT'], name=name+' object create', dependencies=[write], response_model=schema[0]),
+                APIRoute('/'+name, upsert, methods=['PUT'], name=name+' object create', response_model=schema[0]),
                 APIRoute('/'+name+'/{item_id}', one, methods=['GET'], name=name+' object get', response_model=schema[0]),
-                APIRoute('/'+name+'/{item_id}', upsert, methods=['PATCH'], name=name+' object update', dependencies=[write], response_model=schema[0]),
-                APIRoute('/'+name+'/{item_id}', delete, methods=['DELETE'], name=name+' object delete', dependencies=[my], response_model=dict),
+                APIRoute('/'+name+'/{item_id}', upsert, methods=['PATCH'], name=name+' object update', dependencies=[self.oauth.write], response_model=schema[0]),
+                APIRoute('/'+name+'/{item_id}', delete, methods=['DELETE'], name=name+' object delete', dependencies=[self.oauth.my], response_model=dict),
             ])
-            self.app.include_router(ar, prefix=self.prefix, tags=[name], dependencies=[Depends(get_current_user)])
+            self.app.include_router(ar, prefix=self.prefix, tags=[name], dependencies=[Depends(self.oauth.get_current_user)])
 
         # db init
-        load_dotenv()
         register_tortoise(self.app, db_url=env("DB_URL"), modules={"models": [module]}, generate_schemas=debug)
--- tortoise_api/oauth.py
+++ tortoise_api/oauth.py
@@ -1,22 +1,14 @@
 from datetime import timedelta, datetime
 from enum import IntEnum
 from typing import Annotated
-from os import getenv as env
-from dotenv import load_dotenv
 from fastapi import Depends, HTTPException, Security
 from fastapi.security import OAuth2PasswordBearer, SecurityScopes, OAuth2PasswordRequestForm
 from jose import jwt, JWTError
 from pydantic import BaseModel, ValidationError
 from starlette import status
 from tortoise_api_model.enum import Scope, UserRole, UserStatus
-from tortoise_api_model.model import User as UserModel
+from tortoise_api_model.model import Model, User
 from tortoise_api_model.pydantic import UserSchema, UserReg
-
-load_dotenv()
-# to get a string like this run: openssl rand -hex 32
-SECRET_KEY = env('TOKEN')
-ALGORITHM = "HS256"
-EXPIRES = timedelta(days=7)
 
 
 class AuthFailReason(IntEnum):
@@ -25,123 +17,130 @@ class AuthFailReason(IntEnum):
     signature = 3
 
 
-class TokenData(BaseModel):
-    username: str | None = None
-    scopes: list[str] = []
+class OAuth:
+    ALGORITHM = "HS256"
+    EXPIRES = timedelta(days=7)
 
+    class AuthType(IntEnum):
+        pwd = 1
+        tg = 2
 
-class Token(BaseModel):
-    access_token: str
-    token_type: str
-    user: UserSchema
+    db_user_model: Model
+    auth_type: AuthType
 
+    def __init__(self, secret: str, db_user_model: Model = User, auth_type: AuthType = AuthType.pwd):
+        self.secret: str = secret
+        self.db_user_model = db_user_model
+        self.auth_type = auth_type
 
-oauth2_scheme = OAuth2PasswordBearer(
-    tokenUrl="token",
-    scopes={
-        Scope.Read.name: "Read own items",
-        Scope.Write.name: "Write own items",
-        Scope.All.name: "Access for not only own items"
-    }
-)
+    class TokenData(BaseModel):
+        username: str | None = None
+        scopes: list[str] = []
 
+    class Token(BaseModel):
+        access_token: str
+        token_type: str
+        user: UserSchema
 
-# dependency
-async def get_current_user(security_scopes: SecurityScopes, token: Annotated[str | None, Depends(oauth2_scheme)]) -> UserModel:  # , tg_data: [str, ]
-    auth_val = "Bearer"
-    if security_scopes.scopes:
-        auth_val += f' scope="{security_scopes.scope_str}"'
-    cred_exc = HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Could not validate credentials",
-        headers={"WWW-Authenticate": auth_val},
+    oauth2_scheme = OAuth2PasswordBearer(
+        tokenUrl="token",
+        scopes={
+            Scope.Read.name: "Read own items",
+            Scope.Write.name: "Write own items",
+            Scope.All.name: "Access for not only own items"
+        }
     )
-    try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        username: str = payload.get("sub")
-        if not username:
-            cred_exc.detail += 'token'
-            raise cred_exc
-        token_scopes = payload.get("scopes", [])
-        token_data = TokenData(scopes=token_scopes, username=username)
-    except (JWTError, ValidationError) as e:
-        cred_exc.detail += f': {e}'
-        raise cred_exc
-    user = await UserModel.get_or_none(username=token_data.username)
-    if not user:
-        cred_exc.detail = 'User not found'
-        raise cred_exc
-    for scope in security_scopes.scopes:
-        if scope not in token_data.scopes:
-            cred_exc.detail = f'Not enough permissions. Need "{scope}"'
-            raise cred_exc
-    return user
 
-
-# dependency
-async def get_current_active_user(current_user: Annotated[UserModel, Security(get_current_user)]) -> UserModel:
-    if current_user.status < UserStatus.test:
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Inactive user")
-    return current_user
-
-
-read = Security(get_current_active_user, scopes=[Scope.Read.name])
-write = Security(get_current_active_user, scopes=[Scope.Write.name])
-my = Security(get_current_active_user, scopes=[Scope.All.name])
-not_active = Depends(get_current_user)
-
-scopes = {
-    UserRole.Client: [Scope.Read.name],  # read only own
-    UserRole.Agent: [Scope.Read.name, Scope.All.name],  # read all
-    UserRole.Manager: [Scope.Read.name, Scope.Write.name],  # read/write only own
-    UserRole.Admin: [Scope.Read.name, Scope.Write.name, Scope.All.name],  # all
-}
-
-
-# api reg endpoint
-async def reg_user(new_user: UserReg) -> Token:
-    data = new_user.model_dump()
-    try:
-        user: UserModel = await UserModel.create(**data)
-    except Exception as e:
-        raise HTTPException(status.HTTP_406_NOT_ACCEPTABLE, detail=e.__repr__())
-    tok = await login_for_access_token(OAuth2PasswordRequestForm(username=user.username, password=new_user.password))
-    return tok
-
-
-class AuthException(HTTPException):
-    detail: AuthFailReason
-
-    def __init__(
-        self,
-        detail: AuthFailReason,
-    ) -> None:
-        super().__init__(status_code=status.HTTP_401_UNAUTHORIZED, detail=detail.name)
-
-
-async def authenticate_user(username: str, password: str) -> tuple[TokenData, UserModel]:
-    if user_db := await UserModel.get_or_none(username=username):
-        td = TokenData.model_validate(user_db, from_attributes=True)
-        td.scopes = scopes[user_db.role]
-        if user_db.pwd_vrf(password):
-            return td, user_db
-        reason = AuthFailReason.password
-    else:
-        reason = AuthFailReason.username
-    raise AuthException(detail=reason)
-
-
-def gen_access_token(data: dict, expires_delta: timedelta = EXPIRES) -> str:
-    return jwt.encode({"exp": datetime.utcnow() + expires_delta, **data}, SECRET_KEY, algorithm=ALGORITHM)
-
-
-# api login endpoint
-async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]) -> Token:
-    token, user_db = await authenticate_user(form_data.username, form_data.password)
-    if isinstance(token, TokenData):
-        access_token = gen_access_token(
-            data={"sub": token.username, "scopes": token.scopes},
-            expires_delta=EXPIRES,
+    # dependency
+    async def get_current_user(self, security_scopes: SecurityScopes, token: Annotated[str | None, Depends(oauth2_scheme)]) -> Model:  # , tg_data: [str, ]
+        auth_val = "Bearer"
+        if security_scopes.scopes:
+            auth_val += f' scope="{security_scopes.scope_str}"'
+        cred_exc = HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Could not validate credentials",
+            headers={"WWW-Authenticate": auth_val},
         )
-        r = Token.model_validate({"access_token": access_token, "token_type": "bearer", "user": user_db}, from_attributes=True)
-        return r
+        try:
+            payload = jwt.decode(token, self.secret, algorithms=[self.ALGORITHM])
+            username: str = payload.get("sub")
+            if not username:
+                cred_exc.detail += 'token'
+                raise cred_exc
+            token_scopes = payload.get("scopes", [])
+            token_data = self.TokenData(scopes=token_scopes, username=username)
+        except (JWTError, ValidationError) as e:
+            cred_exc.detail += f': {e}'
+            raise cred_exc
+        user = await self.db_user_model.get_or_none(username=token_data.username)
+        if not user:
+            cred_exc.detail = 'User not found'
+            raise cred_exc
+        for scope in security_scopes.scopes:
+            if scope not in token_data.scopes:
+                cred_exc.detail = f'Not enough permissions. Need "{scope}"'
+                raise cred_exc
+        return user
+
+    # dependency
+    @staticmethod
+    async def get_current_active_user(current_user: Annotated[Model, Security(get_current_user)]) -> Model:
+        if current_user.status < UserStatus.test:
+            raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Inactive user")
+        return current_user
+
+    read = Security(get_current_active_user, scopes=[Scope.Read.name])
+    write = Security(get_current_active_user, scopes=[Scope.Write.name])
+    my = Security(get_current_active_user, scopes=[Scope.All.name])
+    not_active = Depends(get_current_user)
+
+    scopes = {
+        UserRole.Client: [Scope.Read.name],  # read only own
+        UserRole.Agent: [Scope.Read.name, Scope.All.name],  # read all
+        UserRole.Manager: [Scope.Read.name, Scope.Write.name],  # read/write only own
+        UserRole.Admin: [Scope.Read.name, Scope.Write.name, Scope.All.name],  # all
+    }
+
+    # api reg endpoint
+    async def reg_user(self, new_user: UserReg) -> Token:
+        data = new_user.model_dump()
+        try:
+            await self.db_user_model.create(**data)
+        except Exception as e:
+            raise HTTPException(status.HTTP_406_NOT_ACCEPTABLE, detail=e.__repr__())
+        tok = await self.login_for_access_token(OAuth2PasswordRequestForm(username=new_user.username, password=new_user.password))
+        return tok
+
+    class AuthException(HTTPException):
+        detail: AuthFailReason
+
+        def __init__(
+            self,
+            detail: AuthFailReason,
+        ) -> None:
+            super().__init__(status_code=status.HTTP_401_UNAUTHORIZED, detail=detail.name)
+
+    async def authenticate_user(self, username: str, password: str) -> tuple[TokenData, Model]:
+        if user_db := await self.db_user_model.get_or_none(username=username):
+            td = self.TokenData.model_validate(user_db, from_attributes=True)
+            td.scopes = self.scopes[user_db.role]
+            if user_db.pwd_vrf(password):
+                return td, user_db
+            reason = AuthFailReason.password
+        else:
+            reason = AuthFailReason.username
+        raise self.AuthException(detail=reason)
+
+    def gen_access_token(self, data: dict, expires_delta: timedelta = EXPIRES) -> str:
+        return jwt.encode({"exp": datetime.utcnow() + expires_delta, **data}, self.secret, algorithm=self.ALGORITHM)
+
+    # api login endpoint
+    async def login_for_access_token(self, form_data: Annotated[OAuth2PasswordRequestForm, Depends()]) -> Token:
+        token, user_db = await self.authenticate_user(form_data.username, form_data.password)
+        if isinstance(token, self.TokenData):
+            access_token = self.gen_access_token(
+                data={"sub": token.username, "scopes": token.scopes},
+                expires_delta=self.EXPIRES,
+            )
+            r = self.Token.model_validate({"access_token": access_token, "token_type": "bearer", "user": user_db}, from_attributes=True)
+            return r
